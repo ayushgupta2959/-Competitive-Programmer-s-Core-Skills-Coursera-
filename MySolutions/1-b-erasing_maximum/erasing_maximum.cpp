f3961daa4764849563ae65d6983848bae82fdeef
ayushgupta2959 <ayushgupta2959@gmail.com>
2018-10-04 20:30:32 +0530

    updated 1-a,1-b

--- MySolutions/1-b-erasing_maximum/erasing_maximum.cpp
+++ MySolutions/1-b-erasing_maximum/erasing_maximum.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 
 using std::cin;
@@ -24,4 +24,50 @@ int main() {
     cout << endl;
 
     return 0;
+}
+*/
+using namespace std;
+#include <bits/stdc++.h>
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,i,max_ele,count,index,flag;
+    int A[101];
+    //while(cin>>n){
+    	//input
+    	cin>>n;
+    	for(i=0;i<n;i++){
+    		cin>>A[i];
+    	}
+    	//compute
+    	max_ele = -1;
+    	for(i=0;i<n;i++){
+    		if(A[i]>max_ele){
+    			max_ele = A[i];
+    			count = 1;
+    			index = i;
+    		}
+    		else if(A[i] == max_ele){
+    			count++;
+    			if(count==3){
+    				index = i;
+    			}
+    		}
+    	}
+    	//output
+        flag=0;
+        if(index!=0){
+            cout<<A[0];
+            flag=1;
+        }
+    	for(i=1;i<n;i++){ 
+    		if(i!=index){
+                if(!flag) flag=1;
+                else cout<<" ";
+                cout<<A[i];
+            }
+    	}
+    	cout<<"\n";
+    //} 
+	return 0;
 }

64f989490fb1be9d0f2d1ce363632d62529415f5
ayushgupta2959 <ayushgupta2959@gmail.com>
2018-10-27 22:43:22 +0530

    solutions added

--- MySolutions/3-a-compare_sums/compare_sums.cpp
+++ MySolutions/3-a-compare_sums/compare_sums.cpp
@@ -1,3 +1,26 @@
+using namespace std;
+#include <bits/stdc++.h>
+int n;
+vector<double> a(n), b(n);
+void read_vector(){
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    for (int i = 0; i < n; ++i)
+        cin >> b[i];
+}
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while(cin >> n){
+        read_vector();
+
+    }
+    return 0;
+}
 #include <iostream>
 #include <vector>
 

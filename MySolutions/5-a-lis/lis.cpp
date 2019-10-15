e0350b191db82c513f98aada7b1761ea78e1e337
SUHAIL AKHTAR <36443650+suhailakhtar039@users.noreply.github.com>
2019-10-15 23:19:57 +0530

    Update lis.cpp

--- MySolutions/5-a-lis/lis.cpp
+++ MySolutions/5-a-lis/lis.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
-#include <vector>
-
-using std::cin;
-using std::cout;
-using std::endl;
-using std::vector;
-
-int main() {
+using namespace std;
+int increasingSubs(int array[],int n)
+{
+    int dp[n+1];
+    for(int i=1;i<=n;i++)
+        dp[i]=1;
+    int maxIN=1;
+    for(int i=1;i<=n;i++)
+    {
+        int ans=0;
+        for(int j=1;j<i;j++)
+        {
+            if(array[i]>array[j])
+            {
+                ans=max(ans,dp[j]);
+            }
+        }
+        dp[i]+=ans;
+        maxIN=max(maxIN,dp[i]);
+    }
+    return maxIN;
+}
+int main()
+{
     int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
+    cin>>n;
+    int array[n+1];
+    for(int i=1;i<=n;i++)
+        cin>>array[i];
 
-    int result = 0;
-    // your code
-
-    cout << result << endl;
-
-    return 0;
+    int ans=increasingSubs(array,n);
+    cout<<ans<<endl;
 }
